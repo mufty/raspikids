a9abb14a8afbad770077b428db33e5931b5da0fc
Michal Gubriansky <gubriansky@sernato.sk>
2016-04-15 18:31:10 +0200

    lib test

--- myWF/components/Pin.js
+++ myWF/components/Pin.js
@@ -6,37 +6,36 @@ components.Pin = {
 
 		if(initData){
 			var inout = gpio.DIR_IN;
-			if(initData.io){
-				if(initData.io == "output")
-					inout = gpio.DIR_IN;
-				
-				gpio.setup(initData.gpioPort, inout, function(){
-					console.log("Pin " + initData.gpioPort + " opened");
-					if(inout == gpio.DIR_IN){
-						gpio.read(initData.gpioPort, function(err, value) {
-							if (err) throw err;
-							
-							if(initData.time){
-								var max_sec = new Date().getTime();
-								while (new Date() < max_sec + initData.time) {}
-							}
-							
-							done();
-					    });
-					} else {
-						gpio.write(7, true, function(err) {
-					        if (err) throw err;
-					        
-					        if(initData.time){
-								var max_sec = new Date().getTime();
-								while (new Date() < max_sec + initData.time) {}
-							}
-							
-							done();
-					    });
-					}
-				});
-			}
+			
+			if(initData.io == "output")
+				inout = gpio.DIR_OUT;
+			
+			gpio.setup(initData.gpioPort, inout, function(){
+				console.log("Pin " + initData.gpioPort + " opened");
+				if(inout == gpio.DIR_IN){
+					gpio.read(initData.gpioPort, function(err, value) {
+						if (err) throw err;
+						
+						if(initData.time){
+							var max_sec = new Date().getTime();
+							while (new Date() < max_sec + initData.time) {}
+						}
+						
+						done();
+				    });
+				} else {
+					gpio.write(initData.gpioPort, initData.value, function(err) {
+				        if (err) throw err;
+				        
+				        if(initData.time){
+							var max_sec = new Date().getTime();
+							while (new Date() < max_sec + initData.time) {}
+						}
+						
+						done();
+				    });
+				}
+			});
 		}
 
 		return {
