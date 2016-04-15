040c177b763df324cd91097ec02437e018b102bf
Michal Gubriansky <gubriansky@sernato.sk>
2016-04-15 18:42:02 +0200

    dasdas

--- myWF/components/Pin.js
+++ myWF/components/Pin.js
@@ -12,30 +12,32 @@ components.Pin = {
 			
 			gpio.setup(initData.gpioPort, inout, function(){
 				console.log("Pin " + initData.gpioPort + " opened");
-				if(inout == gpio.DIR_IN){
-					gpio.read(initData.gpioPort, function(err, value) {
-						if (err) throw err;
-						
-						if(initData.time){
-							var max_sec = new Date().getTime();
-							while (new Date() < max_sec + initData.time) {}
-						}
-						
-						done();
-				    });
-				} else {
-					gpio.write(initData.gpioPort, initData.value, function(err) {
-				        if (err) throw err;
-				        
-				        if(initData.time){
-							var max_sec = new Date().getTime();
-							while (new Date() < max_sec + initData.time) {}
-						}
-						
-						done();
-				    });
-				}
+				
 			});
+			
+			if(inout == gpio.DIR_IN){
+				gpio.read(initData.gpioPort, function(err, value) {
+					if (err) throw err;
+					
+					if(initData.time){
+						var max_sec = new Date().getTime();
+						while (new Date() < max_sec + initData.time) {}
+					}
+					
+					done();
+			    });
+			} else {
+				gpio.write(initData.gpioPort, initData.value, function(err) {
+			        if (err) throw err;
+			        
+			        if(initData.time){
+						var max_sec = new Date().getTime();
+						while (new Date() < max_sec + initData.time) {}
+					}
+					
+					done();
+			    });
+			}
 		}
 
 		return {
