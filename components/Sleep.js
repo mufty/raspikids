ef41633a7e2ddc90eb64849438ca5299bc6949b3
Michal Gubriansky <gubriansky@sernato.sk>
2016-04-30 14:33:55 +0200

    redone the sleep task

--- components/Sleep.js
+++ components/Sleep.js
@@ -1,20 +1,19 @@
-components.Sleep = {
-	init: function(initData, done){
-		console.log("Sleep: " + initData);
+require('./BaseComponent.js');
 
+components.Sleep = class Pin extends BaseComponent {
+	constructor(initData, done, settings){
+		super(initData, done, settings);
+		console.log("Pin init: " + initData);
+		this.clean = false;
+		
+		this._startUp();
+	}
+	_startUp(){
 		if(initData){
 			var max_sec = new Date().getTime();
 			while (new Date() < max_sec + initData.time) {}
-			done();
+			
+			this.handleOutputs();
 		}
-
-		return {
-			inPort: function(){
-				
-			},
-			outPort: function(){
-				
-			}
-		}; 
-	}	
+	}
 };
