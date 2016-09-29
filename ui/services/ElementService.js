e59b2626515dd768610017c71547ef0b44b24bf8
Michal Gubriansky <gubriansky@sernato.sk>
2016-09-29 14:49:35 +0200

    ES6 refactor

--- ui/services/ElementService.js
+++ ui/services/ElementService.js
@@ -1,39 +1,41 @@
-app.service('elementService', function($rootScope) {
-  var selectedElement = {data:null};
-  var selectedLink = {data:null};
-
-  var setSelectedElement = function(newObj, reset) {
-	  //reset selection first
-	  if(!reset)
-		  setSelectedLink(null, true);
+class ElementService {
+	
+	constructor($rootScope) {
+		this.selectedElement = {data:null};
+		this.selectedLink = {data:null};
+		this.rootScope = $rootScope;
+	}
+	
+	setSelectedElement(newObj, reset) {
+		//reset selection first
+		if(!reset)
+			this.setSelectedLink(null, true);
+		  
+		this.selectedElement.data = newObj;
+		this.rootScope.$broadcast('selected_node', newObj);
+	}
+	
+	getSelectedElement() {
+		return this.selectedElement.data;
+	}
+	
+	getSelectedLink() {
+		return this.selectedLink.data;
+	}
+	
+	getSelectedLink() {
+		return this.selectedLink.data;
+	}
 	  
-	  selectedElement.data = newObj;
-	  $rootScope.$broadcast('selected_node', newObj);
-  };
-
-  var getSelectedElement = function(){
-      return selectedElement.data;
-  };
+	setSelectedLink(newObj, reset) {
+		//reset selection first
+		if(!reset)
+			this.setSelectedElement(null, true);
   
-  var getSelectedLink = function(){
-	  return selectedLink.data;
-  }
-  
-  var setSelectedLink = function(newObj, reset){
-	  //reset selection first
-	  if(!reset)
-		  setSelectedElement(null, true);
-	  
-	  selectedLink.data = newObj;
-	  $rootScope.$broadcast('selected_link', newObj);
-  }
-  
+		this.selectedLink.data = newObj;
+		this.rootScope.$broadcast('selected_link', newObj);
+	}
+	
+}
 
-  return {
-	  setSelectedElement: setSelectedElement,
-	  getSelectedElement: getSelectedElement,
-	  setSelectedLink: setSelectedLink,
-	  getSelectedLink: getSelectedLink
-  };
-
-});
+app.service('elementService', ElementService);
